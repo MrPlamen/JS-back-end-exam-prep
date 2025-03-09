b929596da56c04815a102db8015c1d6eaaf17c1d
Plamen Petrov <plamen.veselinov.pterov@gmail.com>
2025-03-10 00:14:54 +0200

    set jwt cookie login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,14 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from "../models/User.js";
+import { JWT_SECRET } from '../../config.js';
 
 const register = async (userData) => {
     if (userData.password !== userData.confirmPassword) {
         throw new Error('Password missmatch!');
     }
 
-    const user = await User.findOne({email: userData.email}).select({_id: true}); // Returns only the id in this way
+    const user = await User.findOne({ email: userData.email }).select({ _id: true }); // Returns only the id in this way
     if (user) {
         throw new Error('User already exists');
     }
@@ -13,8 +16,30 @@ const register = async (userData) => {
     return User.create(userData);
 };
 
+const login = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error('Invalid user or email!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) {
+        throw new Error('Invalid user or email!');
+    }
+
+    const payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+    }
+    const token = jwt.sign(payload, JWT_SECRET)
+
+    return token;
+}
+
 const authService = {
-    register
+    register,
+    login,
 };
 
 export default authService;
